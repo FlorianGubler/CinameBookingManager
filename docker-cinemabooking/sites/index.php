cebdbfa2214a13f642028d061e9d42a0b318bbde
Florian Gubler <70200329+FlorianGubler@users.noreply.github.com>
2021-02-08 23:49:44 +0100

    Structure CSS

--- docker-cinemabooking/sites/index.php
+++ docker-cinemabooking/sites/index.php
@@ -7,8 +7,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Bookingmanager</title>
 </head>
 <body>
-    <h1>-- FUCK YOU --</h1>
+    <div class="login">
+    </div>
+    <header>
+        <nav>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+
+    </footer>
 </body>
