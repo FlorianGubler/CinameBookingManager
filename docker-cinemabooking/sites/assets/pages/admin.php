8c1473a535c10bfe10caaf62ba0bf5eb05167b56
Nino Arisona <nino@arisona.ch>
2021-02-11 15:17:25 +0100

    adminGUI progress 60%

--- docker-cinemabooking/sites/assets/pages/admin.php
+++ docker-cinemabooking/sites/assets/pages/admin.php
@@ -14,7 +14,7 @@ if (isset($_COOKIE['session-id'])) {
     header("Location: ../../index.php");
 }
 $showroom = FALSE;
-if(isset($_POST["room-show"])) {
+if (isset($_POST["room-show"])) {
     $room = $_GET["room"];
 
     foreach ($roomarr as $rooms) {
@@ -32,7 +32,9 @@ if(isset($_POST["room-show"])) {
     }
     $showroom = TRUE;
 }
-
+if (isset($_POST["create-submit"])){
+    echo "fuck"; //easter egg
+}
 
 ?>
 <!DOCTYPE html>
@@ -50,21 +52,69 @@ if(isset($_POST["room-show"])) {
 
 <body>
     <?php include "../page_addon/navbar.php"; ?>
+    <div id="create-user" class="create-user">
+        <form action="" method="post">
+            <a href="admin.php"><i style="float: right; color: gray;" class="fas fa-times"></i></a>
+            <h2>CREATE</h2>
+            <input placeholder="Username" name="username" type="username">
+            <input placeholder="Password" name="password" type="password">
+            <button name="create-submit" type="submit">CREATE</button>
+        </form>
+    </div>
+    <div class="thebigblock"></div>
+    <div class="showroomoverlay">
+        <a href=""><i style="float: right; color: gray;" class="fas fa-times"></i></a>
+        <table>
+            <tr colspan='100%'>
+                <div id='canvas'></div>
+            </tr>
+            <tr>
+                <table class="seats-in-table">
+                    <?php
+                    foreach ($roomseats as $seatrow) {
+                        echo "<tr>";
+                        foreach ($seatrow as $seat) {
+                            echo "<td>";
+                            if (!$seat->except) {
+                                echo "<div class='seat' style='background-color: pink'></div>";
+                            } else {
+                                echo "<div class='seat-empty' ></div>";
+                            }
+                            echo "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    ?>
+                </table>
+            </tr>
+        </table>
+    </div>
     <div class="admin-container">
+
         <a class="back-btn" onclick="history.go(-2)"><i class="fas fa-chevron-left"></i> Zurück</a>
-        <h2>Users</h2>
-        <ul>
-            <?php
-            foreach ($usersarr as $user) {
-                echo "<li>" . $user->id . ": " . $user->username . "</li>";
-            }
-            ?>
-        </ul>
-        <div class="roominfo">
-            <h2>Rooms</h2>
+        <div class="drops">
+
+            <button type='button' class='collapsible'>Users<i style='float:right' class='fas fa-chevron-down'></i></button>
+            <div class='content'>
+                <?php
+
+                echo "<div><form action='#create-user' method='post'>";
+                echo "<button type='submit' name='new-user' class='specialbutton'>CREATE<br>";
+                echo "</form></div>";
+                foreach ($usersarr as $user) {
+                    echo "<div>" . $user->id . ": " . $user->username . "</div>";
+                }
+                ?>
+            </div>
+        </div>
+        <div class="drops">
+
             <button type='button' class='collapsible'>Rooms<i style='float:right' class='fas fa-chevron-down'></i></button>
             <div style='cursor:pointer;' class='content'>
                 <?php
+                echo "<div><form action='createroom.php' method='post'>";
+                echo "<button type='submit' name='createroom.php' class='specialbutton'>CREATE<br>";
+                echo "</form></div>";
                 foreach ($roomarr as $room) {
                     echo "<form action='admin.php?room=" . $room->id . "' method='post'>";
                     echo "<button type='submit' name='room-show'>Room " . $room->number . "<br>";
@@ -74,67 +124,55 @@ if(isset($_POST["room-show"])) {
             </div>
         </div>
         <?php
-        if($showroom==TRUE){
+        if ($showroom == TRUE) {
             echo "<style>.showroomoverlay{display: block}</style>";
         } else {
             echo "<style>.showroomoverlay{display: none}</style>";
         }
         ?>
-        <div class="showroomoverlay">
-            <a href="admin.php" ><i style="float: right; color: gray;"class="fas fa-times"></i></a>
-            <table>
-                <tr colspan='100%'>
-                    <div id='canvas'></div>
-                </tr>
-                <tr>
-                    <table class="seats-in-table">
-                        <?php
-                        foreach ($roomseats as $seatrow) {
-                            echo "<tr>";
-                            foreach ($seatrow as $seat) {
-                                echo "<td>";
-                                if (!$seat->except) {
-                                    echo "<div class='seat' style='background-color: pink'></div>";
-                                } else {
-                                    echo "<div class='seat-empty' ></div>";
-                                }
-                                echo "</td>";
-                            }
-                            echo "</tr>";
-                        }
-                        ?>
-                    </table>
-                </tr>
-            </table>
-        </div>
-        <h2>Movies</h2>
-        <ul>
-            <?php
-            foreach ($moviearr as $movie) {
-                echo "<li><b>" . $movie->name . ":</b> ";
-                echo "<ul>";
-                foreach ($movie->times as $times) {
-                    echo "<li>Room: " . $times->room . " -> " . $times->start . " - " . $times->end . "</li>";
+        <div class="drops">
+
+            <button type='button' class='collapsible'>Movies<i style='float:right' class='fas fa-chevron-down'></i></button>
+            <div style='cursor:pointer;' class="content">
+                <?php
+                echo "<div><form action='addmovie.php' method='post'>";
+                echo "<button type='submit' name='add-movie' class='specialbutton'>CREATE<br>";
+                echo "</form></div>";
+                foreach ($moviearr as $movie) {
+                    echo "<div><form action='#" . urlencode($movie->name) . "' method='post'>";
+                    echo "<button type='submit' name='show-movie'>" . $movie->name . "<br>";
+                    echo "</form></div>";
                 }
-                echo "</ul>";
-                echo "</li>";
+                ?>
+            </div>
+        </div>
+        <?php
+        foreach ($moviearr as $movie) {
+
+            echo "<div class='movieoverlay' id='" . urlencode($movie->name) . "'>";
+            echo "<a href='admin.php'><i style='float: right; color: gray;' class='fas fa-times'></i></a>";
+            echo "<h2>" . $movie->name . "</h2>";
+            foreach ($movie->times as $times) {
+                echo "Room: " . $times->room . " -> " . $times->start . " - " . $times->end . "<br>";
             }
-            ?>
-        </ul>
+            echo "</div>";
+        }
+        ?>
+        <!--
         <h2>Reservations</h2>
         <ul>
-            <?php
+            <?php/*
             foreach ($reservationarr as $reservation) {
-                echo "<li>".$reservation->reservation_user->firstname . ", " . $reservation->reservation_user->lastname . " -> Movie: " . $reservation->movie->name . ", Time: " . $reservation->mv_time->start . " - " . $reservation->mv_time->end;
+                echo "<li>" . $reservation->reservation_user->firstname . ", " . $reservation->reservation_user->lastname . " -> Movie: " . $reservation->movie->name . ", Time: " . $reservation->mv_time->start . " - " . $reservation->mv_time->end;
                 echo "<li><b>Seats: </b>";
                 foreach ($reservation->reservated_seats as $res_seats) {
                     echo $res_seats->row . ";" . $res_seats->col . " / ";
                 }
                 echo "</li>";
                 echo "</li>";
-            }
+            }*/
             ?>
-        </ul>
+        </ul>-->
     </div>
 </body>
 <script>
